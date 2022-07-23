ed860fec67e7db31ce8ce49fc4a554930370e9cd
RyotaIhara <ihara1317@yahoo.co.jp>
2022-07-23 20:24:23 +0900

    最初のコミット

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import App, { Container } from "next/app";
+import Head from "next/head";
+import Link from "next/link";
+import { Nav, NavItem } from "reactstrap";
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Head>
+                <title>フードデリバリーサービス</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" />
+            </Head>
+            <header>
+                <Nav className="navbar navbar-dark bg-dark">
+                    <NavItem>
+                        <Link href="/">
+                            <a className="navbar-brand">ホーム</a>
+                        </Link>
+                    </NavItem>
+                </Nav>
+            </header>
+            <Container>{props.children}</Container>
+        </div>
+    );
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Alert, Button } from 'reactstrap';
+
+const index = () => {
+    return (
+        <div>
+            <div>
+                <Alert color="primary">Hello Project!</Alert>
+                <Button color="primary">Danger!</Button>
+            </div>
+        </div>  
+    )
+}
+
+export default index;
