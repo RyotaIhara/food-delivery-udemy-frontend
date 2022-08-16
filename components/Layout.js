be69dd7485eddd7423d5da18b675e4e9a5758fdc
RyotaIhara <ihara1317@yahoo.co.jp>
2022-08-16 21:32:59 +0900

    アカウント昨日作成

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import App, { Container } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
 import { Nav, NavItem } from "reactstrap";
+import AppContext from "../context/AppContext";
 
 const Layout = (props) => {
+    const { user, setUser } = useContext(AppContext);
+
     return (
         <div className="container">
             <Head>
@@ -17,6 +20,31 @@ const Layout = (props) => {
                         <Link href="/">
                             <a className="navbar-brand">ホーム</a>
                         </Link>
+                    </NavItem>
+                    <NavItem className="ml-auto">
+        
+                        {user ? (
+                            <Link href="/">
+                                <a className="nav-link" onClick={() => {
+                                    setUser(null);
+                                }}>
+                                    ログアウト
+                                </a>
+                            </Link>
+                        ) : (
+                            <Link href="/login">
+                                <a className="nav-link">ログイン</a>
+                            </Link>
+                        )}
+                    </NavItem>
+                    <NavItem>
+                        {user ? (
+                            <h5>{user.username}</h5>
+                        ) : (
+                            <Link href="/register">
+                                <a className="nav-link">ユーザー新規登録</a>
+                            </Link>
+                        )}
                     </NavItem>
                 </Nav>
             </header>
